134d0bebd20b9568c40007a45c004aa85a6dcadd
Ayoub Er rouasse <ayoub.er-rouasse@univ-rouen.fr>
2024-09-10 11:28:16 +0200

    Ajout de prog

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,10 @@
+import fonctions as f
 print("Hello, World !")
+def carre(x):
+	return(x*x)
+while(True):
+	r = input("Donner un entier positif : ")
+	print("Le carré de votre nombre est :", carre(int(r)))
+	a = int(input("Donner le nombre A : "))
+	b = int(input("Donner le nombre B : "))
+	print("A à la puissance de B donne :", f.puissance(a,b))
