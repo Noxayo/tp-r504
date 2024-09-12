f4c52973556e599325c5d972d721d873e017a6b5
Ayoub Er rouasse <ayoub.er-rouasse@univ-rouen.fr>
2024-09-12 09:36:54 +0200

    Update fonctions.py

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -3,5 +3,21 @@ def puissance(a,b):
 		raise TypeError("Only integers are allowed")
 	if not type(b) is int:
 		raise TypeError("Only integers are allowed")
-	r = (a)**(b)
+	
+	if a == 0 and b < 0:
+		x = 0/0
+	if b == 0:
+		return 1
+	if a == 0:
+		return 0
+	
+	r = a
+	
+	if b < 0:
+		for i in range((-b)-1):
+			r = (r)*(a)
+		return 1/r
+	r = a
+	for i in range(b-1):
+		r = (r)*(a)
 	return r
