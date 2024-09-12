9d825c5e7a3477ddfc2fe2282483e9b33108e6c1
Ayoub Er rouasse <ayoub.er-rouasse@univ-rouen.fr>
2024-09-12 09:19:34 +0200

    Update test.py

--- TP1/test.py
+++ TP1/test.py
@@ -3,7 +3,11 @@ import fonctions as f
 def test_1 ( ) :
 	assert f.puissance(2 ,3) == 8
 	assert f.puissance(2 ,2) == 4
-	
+
 def test_2 ( ) :
 	assert f.puissance(-2 ,3) == -8
 	assert f.puissance(10 ,-2) == 0.01
+
+def test_passes_without_info():
+	with pytest.raises(Exception) as e_info:
+		x = f.puissance(0, -5)
